eb7e4b11f71397b12624b71bf7b024d38662ecf9
Eben <bryceaeben@gmail.com>
2026-07-22 21:05:38 -0500

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-22',
   modules: [
     '@tresjs/nuxt'
-  ]
+  ],
+  // Add the lines below:
+  app: {
+    baseURL: '/Kairo-Site/' 
+  },
+  nitro: {
+    preset: 'github-pages'
+  }
 })
